ad1e2c2861d99253aef94e29eaf6ca1f66ec6156
jafar k <jafarkarakkuth@gmail.com>
2024-04-02 15:24:27 +0530

    Add project files.

---
+++ GitTest 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitTest_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
